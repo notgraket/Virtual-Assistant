cc3ac17fee970dd0e189ae2acbfccf81616a2460
notgraket <93066548+notgraket@users.noreply.github.com>
2026-03-16 14:20:49 -0400

    Completely overhauled the old decorator function Now accepts natural language and translates to functions specified during training

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Virtual Assistant
 
+Requirements: Python 3.10+ and the 'transformers' library installed
+
 This is a virtual assistant created to further my understanding of machine learning and llms.
 Currently I'm using HuggingFace's distilgpt2 model that has been fine tuned for function calling.
 As of right now, all it can do is return a function to get the weather status of any city or town in New York.
 
 To get started, download the model and change the directory location in the script to its location on your drive.
 You can run the model in Ollama as normal or you can run the script with the -i flag to manually query it in the command line.
+
+
---
+++ Virtual Assistant/main.py
@@ -0,0 +1,52 @@
+# Nicolas Sepello
+# Interactive Script for the Virtual Assistant
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import requests
+
+model_location = r"H:\Models\distilgpt2-finetuned" # Change to your model's install location
+tokenizer = AutoTokenizer.from_pretrained(model_location)
+model = AutoModelForCausalLM.from_pretrained(model_location)
+
+
+def query(prompt : str):
+    """Returns a valid function given your prompt or a 'I don't know' response"""
+    inputs = tokenizer(prompt, return_tensors = "pt")
+    outputs = model.generate(**inputs, max_length=128, do_sample=True, top_p=0.95, temperature=0.65, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response.split("Assistant: ")[-1].strip() # get only the last output
+
+
+def map_response(response : str):
+    """Dynamically match responses to functions if possible"""
+    response = response.split(" ")
+    response = [segment for segment in response if "(" in segment] # note, train to have a special token to appear if the text contains a function
+    # ["get_weather(...)"] / ["internet_search(...)"] / ...
+    if response[0][:10] == "get_weather":
+        eval(response[0])
+    else:
+        return -1
+
+
+# Callable functions for the model
+
+def get_weather(city, state):
+    """Get the weather data for the city in a given state"""
+    print("called get_weather")
+
+
+# If you run the script without -i this will run
+def main():
+    end = False
+    while not end:
+        user_input = input("Query: ")
+        response = query(user_input)
+        match (response):
+            case -1:
+                print(response)
+            case _:
+                map_response(response)
+
+
+if __name__ == "__main__":
+    main()
